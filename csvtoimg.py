da78feffb1c4d6f968ddbe049638477e695e0f31
LuisBaezN <baez.201041081@gmail.com>
2023-06-04 15:12:04 -0400

    add: data transform script

--- csvtoimg.py
+++ csvtoimg.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+from PIL import Image
+#from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     print("> Program initiated")
     data = pd.read_csv('DB/mnist_train.csv')
 
-    size = 1_000
+    size = 60_000 # 1_000
 
     data = np.array(data)
     m, n = data.shape
 
-    data_sample = data[0:size].T
-    data_filtrated = data_sample[1:n]
-    data_filtrated = data_filtrated / 255.
+    for i in range(size):
+        label = data[i,0]
+        img = data[i,1:]
+        t_img = img.reshape((28,28))
 
-    for i in range(3):
-        current_image = data_filtrated[:, i, None]
-        current_image = current_image.reshape((28, 28)) * 255
-        plt.gray()
-        plt.imshow(current_image, interpolation='nearest')
-        #plt.show()
-        plt.savefig(f'images/{i}.png', bbox_inches='tight')
+        image = Image.fromarray(t_img.astype('uint8'))
+        image.save(f'DBi/img{i+1}-{label}.jpg')
+        #image.show()
+
+        porcentaje = ((i+1)*100)/size
+        if porcentaje % 5 == 0:
+            print(f'{int(porcentaje)}%')
